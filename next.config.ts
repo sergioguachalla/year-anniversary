8ae78c5e7b76c4263b516df2177418c62f5f4f1b
Sergio Guachalla <sergioguachalla0@gmail.com>
2025-10-21 15:06:26 -0400

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
+import { config } from "process";
 
 
 const isProd = process.env.NODE_ENV === "production";
 const repo = "year-anniversary";
 
-const config: NextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : undefined,
   assetPrefix: isProd ? `/${repo}/` : undefined,
 };
-export default config;
+export default nextConfig;
