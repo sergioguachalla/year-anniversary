62ad81eb9d6318984327c68c7f91d42e7e3ec0d2
Sergio Guachalla <sergioguachalla0@gmail.com>
2025-10-23 13:08:20 -0400

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,14 @@
-import type { NextConfig } from "next";
-import { config } from "process";
-
-
+// next.config.ts
 const isProd = process.env.NODE_ENV === "production";
 const repo = "year-anniversary";
 
-const nextConfig: NextConfig = {
+export default {
   output: "export",
   images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : undefined,
   assetPrefix: isProd ? `/${repo}/` : undefined,
   env: {
-    // para usar en el cliente
     NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "",
   },
-  // opcional pero útil en GH Pages (URLs con / al final)
-  trailingSlash: true,
-};
-
-export default nextConfig;
+  trailingSlash: true, // opcional, ayuda en GH Pages
+} satisfies import("next").NextConfig;
